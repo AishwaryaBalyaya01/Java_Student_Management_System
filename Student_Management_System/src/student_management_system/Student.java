e7821b043ff29c62ddbda11695f7c91b2c58c6ba
AishwaryaBalyaya01 <Aishwarya.balyaya19@gmail.com>
2023-06-08 16:52:07 -0400

    Added classes

---
+++ Student_Management_System/src/student_management_system/Student.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student_management_system;
+
+/**
+ *
+ * @author aishw
+ */
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tutionbalance;
+    private int costOfCourse  =600;
+    //Constructor: Prompts user to enter stuent'Name & Year
+    
+    //Generate an Id
+    
+    //Enroll in Courses
+    
+    //View balance and pay Tution
+    
+    //Show Status
+}
